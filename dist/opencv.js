850963d97dfd54c4e93954e19c6f21a9ec824181
kyoujuro <shakemaru0138550@gmail.com>
2022-01-01 17:43:36 +0900

    add

--- dist/opencv.js
+++ dist/opencv.js
@@ -6,4 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const opencv_ts_1 = __importDefault(require("opencv-ts"));
 opencv_ts_1.default.onRuntimeInitialized = () => {
     const src = opencv_ts_1.default.imread("inputCanvas");
+    const dst = new opencv_ts_1.default.Mat(src.cols, src.rows, opencv_ts_1.default.CV_8UC4);
+    opencv_ts_1.default.resize(src, dst, new opencv_ts_1.default.Size(500, 500), 0, 0, opencv_ts_1.default.INTER_AREA);
+    const roiRect = new opencv_ts_1.default.Rect(0, 0, 200, 200);
+    const roi = dst.roi(roiRect);
+    opencv_ts_1.default.imshow('outputCanvas', roi);
 };
